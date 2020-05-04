feb846c173cb9d8086a145f90a97a431ff82a052
TEROMO <44552193+TEROMO@users.noreply.github.com>
2020-05-04 14:21:56 +0500

    1

---
+++ MobileAppAR/Assets/Scripts/Back.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Back : MonoBehaviour
+{
+    public string sceneName;
+    public Button backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ MobileAppAR/Assets/Scripts/Play.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Play : MonoBehaviour
+{
+    public string sceneName;
+    public Button playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener (TaskOnClick); 
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        SceneManager.LoadScene(sceneName); 
+    }
+}
